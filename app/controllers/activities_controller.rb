0b7d56d44aec994e27d84cc8d706be6a8e4b48cf
jasonblanchard <jas.blanchard@gmail.com>
2013-04-03 13:31:39 -0400

    Added identifyers to words

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,11 +5,37 @@ class ActivitiesController < ApplicationController
 
         @reverse_text = ''
 
+        @markup_text = ''
+
         if params[:t]
-            params[:t].lines.each do |line|
+            @markup_text = add_markup(@text)
+        end
+
+        if params[:t]
+            @text.lines.each do |line|
                 @reverse_text += line.split(' ').reverse.join(' ') + "\r\n"
             end
         end
+
+        @reverse_text = add_markup(@reverse_text, true)
+
     end
 
+    protected
+
+    def add_markup(input, reverse=false)
+        output = ''
+        input.lines.each do |line|
+            output += line.split.map { |word| "<a href='#' class='#{get_word_identifier(line.split, word, reverse)}'>#{word}</a>" }.join(' ') + "\r\n"
+        end
+        output
+    end
+
+    def get_word_identifier(array, value, reverse=false)
+        if reverse
+            (array.length - array.index(value)) - 1
+        else
+            array.index(value)
+        end
+    end
 end
