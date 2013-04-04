4146eefd3cc546ecdba762c5dd2f219a119c2f49
jasonblanchard <jas.blanchard@gmail.com>
2013-04-04 16:53:07 -0400

    Added passing test for checking topic sentences

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,33 @@
+Given(/^I am on the "(.*?)" page$/) do |arg1|
+    visit "/#{arg1}"
+end
+
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |field, input|
+    fill_in field, :with => input
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+    page.should have_content(arg1)
+end
+
+
+Then(/^I should not see "(.*?)"$/) do |arg1|
+    page.should have_no_content(arg1)
+end
+
+Given(/^I press "(.*?)"$/) do |arg1|
+    click_button arg1
+end
+
+Then(/^show page$/) do
+    puts page.body
+end
+
+Given(/^I send "(.*?)" as "(.*?)" to "(.*?)"$/) do |param, input, path|
+    input.gsub!("\\r\\n","\r\n")
+    Capybara.current_session.driver.submit :post, "/#{path}", param => input
+end
+
+Then(/^I should see list item "(.*?)"$/) do |content|
+    page.body.include?("<li>" + content + " </li>").should be_true
+end
