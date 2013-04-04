972ffa6832b167912a09d7700c8b9a625ede673b
jasonblanchard <jas.blanchard@gmail.com>
2013-04-04 14:22:17 -0400

    Fixed reverse sentences bug

--- app/controllers/reverse_sentences_controller.rb
+++ app/controllers/reverse_sentences_controller.rb
@@ -2,9 +2,11 @@ class ReverseSentencesController < ApplicationController
     def reverse_sentences
         @text = params[:t]
 
-        @text.gsub!(/[.] (\d+)/, '# \1')
-        @sentences = @text.split(/[.!?]['"]* |$/)
-        @sentences.each { |sentence| sentence.chomp }.reject! { |c| c.blank? }
+        if params[:t]
+            @text.gsub!(/[.] (\d+)/, '# \1')
+            @sentences = @text.split(/[.!?]['"]* |$/)
+            @sentences.each { |sentence| sentence.chomp }.reject! { |c| c.blank? }
+        end
     end
 
 end
