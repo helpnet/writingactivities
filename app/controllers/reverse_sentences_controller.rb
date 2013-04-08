98b26f462f41e106b19ee40f4a154a835ca8b6a0
jasonblanchard <jas.blanchard@gmail.com>
2013-04-08 11:44:47 -0400

    Refactored using scan to better capture sentences

--- app/controllers/reverse_sentences_controller.rb
+++ app/controllers/reverse_sentences_controller.rb
@@ -3,7 +3,7 @@ class ReverseSentencesController < ApplicationController
         @text = params[:t]
 
         if params[:t]
-            @sentences = Activity::sanitize_dots(@text.dup).split(/[.!?]['"]* |$/)
+            @sentences = Activity::sanitize_dots(@text.dup).scan(/([\w [^.!?]]+(?:."* *|\z|$))/).flatten
             @sentences.each { |s| s.chomp! }.each { |s| s.chomp! }.delete_if { |s| s.empty? }.map { |sentence| Activity::restore_dots!(sentence) }
         end
     end
--- app/controllers/topic_sentences_controller.rb
+++ app/controllers/topic_sentences_controller.rb
@@ -6,7 +6,7 @@ class TopicSentencesController < ApplicationController
         @lines = []
 
         @text.lines.each do |line|
-            sentence = Activity::sanitize_dots(line).split(/([.!?]['"]* )/).each_slice(2).map(&:join)[0]
+            sentence = Activity::sanitize_dots(line).scan(/([\w [^.!?]]+(?:."* *|\z|$))/).flatten[0]
             Activity::restore_dots!(sentence)
             @topic_sentences << sentence if sentence.chomp != ""
         end
