6023ba0bf27da7ce7976ce987be63c5fe9791877
jasonblanchard <jas.blanchard@gmail.com>
2013-04-08 12:29:56 -0400

    Working on exceptions to sentence rule

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ActiveRecord::Base
 
     def self.sanitize_dots(string)
-        string.gsub(/[.]( *\d+)/, '##\1').gsub(/(al|Mr|Ms|Dr|Mrs|vs|Vs|i.e|I.e|ex|EX|e.x|E.X)[.]["|'|,]* /, '\1## ')
+        string.gsub(/[.]( *\d+)/, '##\1').gsub(/(al|mr|ms|dr|mrs|vs|ie|eg|ex|ex)[.][|"|'|,]* /i, '\1## ').gsub(/[.]([a-z|A-Z]+)/, '##\1').gsub(/([a-zA-Z#]+)[.]( [a-z])/, '\1##\2')
     end
 
     def self.restore_dots!(string)
