ea1e6ff79bf1e074b38d87e17852ece9da945edd
jasonblanchard <jas.blanchard@gmail.com>
2013-04-08 09:40:27 -0400

    Fixed overlapping gsub bugs

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,8 +3,7 @@ class Activity < ActiveRecord::Base
     protected
 
     def self.sanitize_dots(string)
-        string.gsub(/[.] (\d+)/, '## \1')
-        string.gsub(/(Mr|Ms|Dr|Mrs|vs|Vs|i.e|I.e|ex|EX|e.x|E.X)[.] /, '\1## ')
+        string.gsub(/[.] (\d+)/, '## \1').gsub(/(Mr|Ms|Dr|Mrs|vs|Vs|i.e|I.e|ex|EX|e.x|E.X)[.] /, '\1## ')
     end
 
     def self.restore_dots!(string)
