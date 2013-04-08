a6e1e0d68fc846a4ec5fca1742182041c379e117
jasonblanchard <jas.blanchard@gmail.com>
2013-04-08 13:11:03 -0400

    Refactored gsub sentence exceptions

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,10 @@
 class Activity < ActiveRecord::Base
 
     def self.sanitize_dots(string)
-        string.gsub(/[.]( *\d+)/, '##\1').gsub(/(al|mr|ms|dr|mrs|vs|ie|eg|ex|ex)[.][|"|'|,]* /i, '\1## ').gsub(/[.]([a-z|A-Z]+)/, '##\1').gsub(/([a-zA-Z#]+)[.]( [a-z])/, '\1##\2')
+        string.gsub(/[.]( *\d+)/, '##\1')
+        .gsub(/(al|mr|ms|dr|mrs|vs|ie|eg|ex|ex)[.][|"|'|,]* /i, '\1## ')
+        .gsub(/[.]([a-z|A-Z]+)/, '##\1')
+        .gsub(/([a-zA-Z#]+)[.]( [a-z])/, '\1##\2')
     end
 
     def self.restore_dots!(string)
