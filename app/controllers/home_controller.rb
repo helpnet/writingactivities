03aa7202238158cf0a4a5050ea285b46510fda59
jasonblanchard <jas.blanchard@gmail.com>
2013-04-10 15:25:25 -0400

    Showing LTI info on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
     
     def home
+        if session['launch_params']
+            key = session['launch_params']['oauth_consumer_key']
+            @tp = IMS::LTI::ToolProvider.new(key, $oauth_creds[key], session['launch_params'])
+        end
     end
 
     def sample_text
