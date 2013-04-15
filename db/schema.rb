c937f8015e07e315c84d3c315f187a9bb31a8591
jasonblanchard <jas.blanchard@gmail.com>
2013-04-15 13:13:35 -0400

    Added role to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415163349) do
+ActiveRecord::Schema.define(:version => 20130415171057) do
 
   create_table "activities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20130415163349) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
