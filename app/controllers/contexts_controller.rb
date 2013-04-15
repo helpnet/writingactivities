9cda1dc6779b31866bb9bfbc712735bafb9e01cf
jasonblanchard <jas.blanchard@gmail.com>
2013-04-15 14:46:36 -0400

    Added role message to context view

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -1,5 +1,6 @@
 class ContextsController < ApplicationController
     def show
         @context = Context.find(params[:id])
+        @role = current_user.memberships.where("context_id == #{@context.id}").first.role
     end
 end
