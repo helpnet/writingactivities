90732268528385ebf06157b0cf69683bb3b49814
jasonblanchard <jas.blanchard@gmail.com>
2013-04-23 15:22:40 -0400

    Added fake upvote buttons

---
+++ app/assets/javascripts/peer_evaluations.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.helpful-tooltip').tooltip();
+
+    $('.helpful-tooltip').click(function() {
+        return false;
+    });
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+    def helpful_tooltip
+        "<a href='#' data-toggle='tooltip' title='This was helpful' class='helpful-tooltip'><i class='icon-thumbs-up'></i></a>"
+    end
 end
