b87e0878b096f2222fbe4c01256377646e77622a
jasonblanchard <jas.blanchard@gmail.com>
2013-04-25 15:07:29 -0400

    Writing some sanity tests

---
+++ features/add_activity.feature
@@ -0,0 +1,34 @@
+Feature: Adding an activity to a course
+    As an instructor
+    So that I can prompt the students to submit writing samples
+    I want to add and edit activities
+
+    Background:
+        Given the following contexts exist:
+            | context_label | context_title     |
+            | TC101         | Test Course 101   |
+
+        Given the following users exist:
+            | email             | password  |
+            | ralph@example.com | testpass  |
+            | prof@example.com  | testpass  |
+            | sally@example.com | testpass  |
+
+        Given the following memberships exist:
+            | user                  | context   | role      |
+            | ralph@example.com     | TC101     | learner   |
+            | prof@example.com      | TC101     | instructor|
+            | sally@example.com     | TC101     | learner   |
+
+    Scenario: Adding a new activity with a basic review type
+        Given "ralph@example.com" is logged in
+        And I am on the context page for "TC101"
+        And I click "Add a New Activity"
+        And I fill in "prompt_title" with "Submit An Idea"
+        And I fill in "prompt_body" with "Your idea should include good stuff"
+        And I select the "basic_reviews" checkbox
+        And I press "Create Prompt"
+        Then I should see "Submit An Idea"
+        And I click "Submit An Idea"
+        Then I should see "Your idea should include good stuff"
+        And I should see "Available review types: basic_reviews"
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,3 +1,25 @@
+Given(/^the following contexts exist:$/) do |table|
+    table.hashes.each do |context|
+        Context.create( context_label: context[:context_label], context_title: context[:context_label] )
+    end
+end
+
+Given(/^the following users exist:$/) do |table|
+    table.hashes.each do |user|
+        User.create( email: user[:email], password: user[:password] )
+    end
+end
+
+Given(/^the following memberships exist:$/) do |table|
+    table.hashes.each do |membership|
+        user = User.find_by_email(membership[:user])
+        context = Context.find_by_context_label(membership[:context])
+        role = membership[:role]
+
+        Membership.create( user_id: user.id, context_id: context.id, role: role )
+    end
+end
+
 Given(/^I am on the "(.*?)" page$/) do |arg1|
     visit "/#{arg1}"
 end
@@ -34,4 +56,25 @@ end
 
 Then(/^I should see html "(.*?)"$/) do |arg1|
     page.body.include?(arg1).should be_true
+end
+
+Given(/^I am on the context page for "(.*?)"$/) do |context_label|
+    context = Context.find_by_context_label(context_label)
+    visit context_url(context)
+    assert page.should have_content("#{context.context_label} #{context.context_title}")
+end
+
+Given(/^I click "(.*?)"$/) do |link|
+    click_link link
+end
+
+Given(/^I select the "(.*?)" checkbox$/) do |checkbox|
+    check checkbox
+end
+
+Given(/^"(.*?)" is logged in$/) do |email|
+    visit '/users/sign_in'
+    fill_in "user_email", :with => email
+    fill_in "user_password", :with => 'testpass'
+    click_button "Sign in"
 end
