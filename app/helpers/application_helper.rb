7b80d8f26b14a07e5eb3b60f99926c2ee58b900a
jasonblanchard <jas.blanchard@gmail.com>
2013-04-25 11:22:51 -0400

    Added helper to check boxes by default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,15 @@ module ApplicationHelper
         flag = current_context.context_label == context.context_label ? "<span class='label label-warning'>LTI</span> " : ''
 
         link_to raw("#{flag}#{context.context_label} #{context.context_title}"), context
-
     end
+
+    def get_review_type_form_state(prompt, review_type_id)
+        if prompt.review_types.map(&:review_type).include?(review_type_id.to_s)
+            1
+        else
+            nil
+        end
+    end
+
 
 end
