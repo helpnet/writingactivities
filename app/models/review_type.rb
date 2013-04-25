be9ce7442336fe2f6167ee2cb61755c2cf4f9eae
jasonblanchard <jas.blanchard@gmail.com>
2013-04-25 11:06:04 -0400

    Added human name to hash

--- app/models/review_type.rb
+++ app/models/review_type.rb
@@ -2,7 +2,7 @@ class ReviewType < ActiveRecord::Base
   belongs_to :prompt
   attr_accessible :review_type
 
-  @@review_types = [:basic_reviews]
+  @@review_types = {:basic_reviews => "Basic Review"}
 
   def self.review_types
       @@review_types
