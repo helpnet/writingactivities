0f1f2baaaeefd67e58090e9d9df06a0a0b031893
jasonblanchard <jas.blanchard@gmail.com>
2013-05-09 15:11:37 -0400

    Minor refactoring

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -5,17 +5,13 @@ class Consumer < ActiveRecord::Base
 
   def self.authorize!(params, request)
 
-        @valid_lti = false
-
         if key = params['oauth_consumer_key']
             if @consumer = Consumer.find_by_key(key) and secret = @consumer.secret
                 @tp = IMS::LTI::ToolProvider.new(key, secret, params)
-                @valid_lti = true
             else
                 @tp = IMS::LTI::ToolProvider.new(nil, nil, params)
                 @tp.lti_msg = "Your consumer didn't use a recognized key."
                 @tp.lti_errorlog = "You did it wrong!"
-                @valid_lti = false
                 return @tp
             end 
 
@@ -26,26 +22,20 @@ class Consumer < ActiveRecord::Base
 
         if !@tp.valid_request?(request)
             @tp.lti_msg = "The OAuth signature was invalid."
-            @valid_lti = false
             return @tp
         end 
 
         if Time.now.utc.to_i - @tp.request_oauth_timestamp.to_i > 60*60
-            @valid_lti = false
             @tp.lti_msg = "Request is too old"
             return @tp
         end 
 
         if !nonce_is_valid?(@tp.request_oauth_nonce, @tp.request_oauth_timestamp)
-            @valid_lti = false
             @tp.lti_msg = "Nonce was already used, please try again."
             return @tp
         end 
 
-        if @valid_lti
-            @tp.lti_msg = 'success'
-        end 
-
+        @tp.lti_msg = 'success'
         @tp
 
     end 
